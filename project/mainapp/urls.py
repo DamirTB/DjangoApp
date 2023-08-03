7899269a34f504efbc78885ae23d67aa3bd775c2
DamirTB <talipdamir@gmail.com>
2023-08-03 18:30:43 +0500

    Implemented a login & register system into application

--- project/mainapp/urls.py
+++ project/mainapp/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path 
+from django.urls import path, include
 from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path("account/", include("django.contrib.auth.urls")),
+    path("signup/", views.SignUp.as_view(), name="signup"),
+    path("logout/", views.logout_request, name="logout"),
     path('delete/<int:expense_id>/', views.delete_expense, name='delete_exp'),
     path('create/', views.create_expense.as_view(), name="create_expense"),
 ]
--- project/mainapp/views.py
+++ project/mainapp/views.py
@@ -1,39 +1,44 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Expense
 from .forms import ExpenseForm
 
+@login_required
 def index(request):
-    if request.method=="POST":
-        form = ExpenseForm(request.POST)
-        if form.is_valid():
-            new_expense = Expense(amount=form.cleaned_data['amount'], category=form.cleaned_data['category'])
-            new_expense.save()
-            return redirect('/')
-    else: 
-        form = ExpenseForm()
     expense_list = Expense.objects.all()
     sum_groceries = Expense.sum_amount("Groceries")
     sum_rent = Expense.sum_amount("Rent")
     sum_other = Expense.sum_amount("Other")
     sum_taxes = Expense.sum_amount("Taxes")
-    context = {"form" : form ,
-               "obj_list" : expense_list ,
+    context = {"obj_list" : expense_list ,
                "sum_gr" : sum_groceries , 
                "sum_rt" : sum_rent, 
                "sum_ot" : sum_other,
                "sum_tx" : sum_taxes}
     return render(request, "mainapp/index.j2", context)
 
-class create_expense(CreateView):
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name="registration/register.html"
+    success_url = reverse_lazy("index")
+
+@login_required
+def logout_request(request):
+    logout(request)
+    return redirect("index")
+
+class create_expense(LoginRequiredMixin, CreateView):
     model = Expense
     form_class = ExpenseForm
     template_name = "mainapp/expense_create.html"
 
+@login_required
 def delete_expense(request, expense_id):
     try:
         expense = Expense.objects.get(id=expense_id)
